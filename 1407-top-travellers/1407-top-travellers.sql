0fb59545c18975f5a477febac3209caea736b2a0
azizjonhamroyev <68224919+azizjonhamroyev@users.noreply.github.com>
2023-01-06 13:51:01 +0500

    Time: 2490 ms (5.02%), Space: 0B (100.00%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,6 @@
+select u.name,
+       coalesce(sum(r.distance),0) as travelled_distance
+from users u
+left join rides r on u.id = r.user_id
+group by r.user_id, u.name
+order by travelled_distance desc ,u.name
