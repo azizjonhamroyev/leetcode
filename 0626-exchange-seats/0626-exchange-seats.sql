f25f82b4a13b7bf48150b2db4649a945f09d1860
azizjonhamroyev <68224919+azizjonhamroyev@users.noreply.github.com>
2023-01-09 03:09:45 +0500

    Time: 271 ms (90.72%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,8 +1,3 @@
-select id,
-       case
-           when id % 2 = 0
-               then (select s1.student from seat s1 where s1.id = s.id - 1)
-           when id % 2 = 1 and (select s2.student from seat s2 where s2.id = s.id + 1) is not null
-               then (select s2.student from seat s2 where s2.id = s.id + 1)
-           else student end as student
-from seat s
+SELECT ROW_NUMBER() OVER() id, student
+FROM seat
+ORDER BY IF(MOD(id, 2) = 0, id-1, id+1)
